560cf51b06c932afdea6f2b77766bb5f228cd67c
Carlitcs CS <carlitcscs@Carlitcss-MacBook-Pro.local>
2026-04-29 19:35:26 -0500

    Add apps in settings

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    
+    'apps.chat',
+    'apps.users',
+    'apps.ai',
 ]
 
 MIDDLEWARE = [
