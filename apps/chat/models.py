70566e8c829e87e0ab0b700a61be86a94125f630
Carlitcs CS <carlitcscs@Carlitcss-MacBook-Pro.local>
2026-04-29 19:40:21 -0500

    Add conversation model

--- apps/chat/models.py
+++ apps/chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Conversation(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="conversations"
+    )
+
+    title = models.CharField(max_length=255, blank=True, null=True)
+    summary = models.TextField(blank=True, null=True)
+    summary_last_message_id = models.IntegerField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Conversation {self.id}"
